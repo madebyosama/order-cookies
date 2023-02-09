c76b961b548146e231ef336a122d16dddf2ff480
codebyosama <codebyosama@gmail.com>
2023-02-09 18:46:33 +0500

    Cart added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { useEffect, useState } from 'react';
 export default function App() {
   const [cookies, setCookies] = useState([]);
   const [selectedCookies, setSelectedCookies] = useState([]);
-  const [selectedNumber, setSelectedNumber] = useState();
-  const [numberOfCookies, setNumberOfCookies] = useState();
+  const [selectedNumber, setSelectedNumber] = useState(0);
+  const [numberOfCookies, setNumberOfCookies] = useState(6);
+  const [cartCookies, setCartCookies] = useState([]);
 
   useEffect(() => {
     async function fetch() {
@@ -17,22 +18,45 @@ export default function App() {
     fetch();
   }, []);
 
+  useEffect(() => {
+    setCartCookies([...new Set(selectedCookies.map((x) => x))]);
+    console.log(cartCookies);
+  }, [selectedCookies]);
+
+  const checkCount = (c) => {
+    return selectedCookies.filter((d) => d.title === c.title).length;
+  };
+
   return (
     <div className='flex'>
       <div className='grow-2 cookies-column'>
         <div className='cookies-header'>
+          <p className='light-font'>Select 6 Cookies of your Choice</p>
           <h3 className='text-center title'>Build your own box</h3>
-          <p className='light-font'>Select your box size:</p>
-          <select className='select-cookies'>
+          {/* <select className='select-cookies'>
             <option>4 Pack</option>
-          </select>
+          </select> */}
           <p className='light-font'>Click on a cookie to add it to your box</p>
         </div>
         <div className='cookies'>
           {cookies.map((c) => {
             return (
-              <div className='cookie'>
-                <img src={c.image} width='160px' />
+              <div
+                className='cookie'
+                onClick={() => {
+                  if (selectedNumber < numberOfCookies) {
+                    setSelectedCookies([...selectedCookies, c]);
+                    setSelectedNumber(selectedNumber + 1);
+                  }
+                  checkCount(c);
+                }}
+              >
+                {checkCount(c) ? (
+                  <div className='cookie-counter'>{checkCount(c)}</div>
+                ) : (
+                  <div></div>
+                )}
+                <img src={c.image} width='130px' />
                 <p className='cookie-title'>{c.title}</p>
               </div>
             );
@@ -45,7 +69,28 @@ cookie-gathering-column'
       >
         <div className='cookies-header'>
           <h3 className='text-center title box-title'>Your Box</h3>
+          <p className=''>{selectedNumber + '/' + numberOfCookies}</p>
           <button className='add-to-cart-btn'>Add to Cart</button>
+        </div>
+        <div className='cart-cookies'>
+          {cartCookies.map((c) => {
+            return (
+              <div
+                className='cart-cookie'
+                onClick={() => {
+                  if (selectedNumber < numberOfCookies) {
+                    setSelectedCookies([...selectedCookies, c]);
+                    setSelectedNumber(selectedNumber + 1);
+                  }
+                  checkCount(c);
+                }}
+              >
+                <div className='cookie-counter'>{checkCount(c)}</div>
+                <img src={c.image} width='130px' />
+                <p className='cookie-title'>{c.title}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
